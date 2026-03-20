577ff727341ad1fd4405cc9ee4395a4adbbcc16a
Meet Parmar <101337115+H2kM33t@users.noreply.github.com>
2026-03-20 12:45:12 +0530

    Refactor main.cpp for CT and PT readings
    Updated the main.cpp to include potential transformer readings and modified the setup and loop functions accordingly.

--- ENERGY_METER_IOT/src/main.cpp
+++ ENERGY_METER_IOT/src/main.cpp
@@ -1,29 +1,39 @@
 #include <Arduino.h>
-#define DT_OUTPUT_PIN 34
 
-#define CURRENT_PER_VOLT 0.55 // Ampere from Clampmeter/Output Votage from CT
+// CURRENT TRANSFORMER OUTPUT PIN
+int CT_PIN = 32;
+
+// POTIENTIAL TRANSFORMER OUTPUT PIN
+int PT_PIN = 33;
 
 void setup()
 {
+  // SERIAL MONITOR CONFIGURATION
   Serial.begin(115200);
 
-  analogReadResolution(12);       // 0–4095
-  analogSetAttenuation(ADC_11db); // 0–3.3 V
+  analogReadResolution(12);       // Set 12-bit resolution (0–4095)
+  analogSetAttenuation(ADC_11db); // USE THE FULL 0.0V - 3.3 V
 }
 
-void loop()
+void main()
 {
-  int adcValue = analogRead(DT_OUTPUT_PIN);
 
-  // Voltage calculation
-  float voltage = adcValue * (3.3 / 4095.0) + 0.150; // 150 OFFSET
+  int current_adc = analogRead(CT_PIN);
+  int voltage_adc = analogRead(PT_PIN);
 
-  // Convert voltage to current
-  float current = voltage * CURRENT_PER_VOLT;
+  // Convert to voltage
+  float current_voltage = (3.3 / 4095.0) * current_adc + 0.150;
+  float voltage_voltage = (3.3 / 4095.0) * voltage_adc;
 
-  Serial.print("Current: ");
-  Serial.print(current, 1); // <<< ONLY 1 digit after decimal
-  Serial.println(" A");
+  Serial.print("CT ADC: ");
+  Serial.print(current_adc);
+  Serial.print(" | CT Voltage: ");
+  Serial.print(current_voltage);
 
-  delay(1000);
+  Serial.print(" || PT ADC: ");
+  Serial.print(voltage_adc);
+  Serial.print(" | PT Voltage: ");
+  Serial.println(voltage_voltage);
+
+  delay(500);
 }
