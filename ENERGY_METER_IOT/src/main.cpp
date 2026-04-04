9fc7902e13cbb85d278bedf67d9b4d6f39d3e09a
Meet Parmar <101337115+H2kM33t@users.noreply.github.com>
2026-04-04 14:24:32 +0530

    main.cpp

--- ENERGY_METER_IOT/src/main.cpp
+++ ENERGY_METER_IOT/src/main.cpp
@@ -1,13 +1,13 @@
 #include <Arduino.h>
 
-// PINS
+// ===== PINS =====
 #define VOLTAGE_PIN 33
 #define CT_PIN 32
 
-// SETTINGS
+// ===== SETTINGS =====
 #define NUM_SAMPLES 500
 #define VOLTAGE_SCALING 93.56   // Voltage calibration
-#define CURRENT_SCALING 0.7   // Current calibration
+#define CURRENT_SCALING 0.525     // Current calibration
 
 void setup()
 {
@@ -38,15 +38,17 @@ void loop()
 
   // ===== Current Calculation =====
   int current_adc = analogRead(CT_PIN);
-  float current_voltage = (3.3 / 4095.0) * current_adc + 0.150;
-
+  float current_voltage = (3.3 / 4095.0) * current_adc;
   float current_actual = current_voltage * CURRENT_SCALING;
+
+  // ===== Rounding to 1 Decimal Place =====
+  float current_rounded = round(current_actual * 10) / 10.0;
 
   // ===== Output =====
   Serial.print("Mains Voltage: ");
   Serial.print(Vrms_mains, 1);
   Serial.print(" V | Current: ");
-  Serial.print(current_actual, 3);
+  Serial.print(current_rounded, 1);
   Serial.println(" A");
 
   delay(1000);
